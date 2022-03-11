e90d39dd7a4d1270ae1bfe0f0fad0365ab3ca851
Yusung <qkrdbtjd15@naver.com>
2022-03-11 14:50:27 +0900

    파라미터로 받은 dealDate 의 Validator

---
+++ house-batch/src/main/java/com/yusung/housebatch/job/validator/DealDateParameterValidator.java
@@ -0,0 +1,28 @@
+package com.yusung.housebatch.job.validator;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class DealDateParameterValidator implements JobParametersValidator {
+
+    private static final String DEAL_DATE = "dealDate";
+
+    @Override
+    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+        String dealDate = parameters.getString(DEAL_DATE);
+        if(!StringUtils.hasText(dealDate)){
+            throw new JobParametersInvalidException(DEAL_DATE + "가 빈 문자열이거나 존재하지 않습니다.");
+        }
+
+        try{
+            LocalDate.parse(dealDate);
+        }catch (DateTimeParseException e){
+            throw new JobParametersInvalidException(DEAL_DATE +"가 올바른 날짜형식이 아닙니다. yyyy-MM-dd 이어야 합니다.");
+        }
+    }
+}
