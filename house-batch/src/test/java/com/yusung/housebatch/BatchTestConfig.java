954d730fe208a613282310f6510e439cf6ea631e
Yusung <qkrdbtjd15@naver.com>
2022-03-11 14:49:05 +0900

    배치 테스트 환경 설정

---
+++ house-batch/src/test/java/com/yusung/housebatch/BatchTestConfig.java
@@ -0,0 +1,12 @@
+package com.yusung.housebatch;
+
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class BatchTestConfig {
+}
