2ae26825bea9dd6d773fb0b1d836396e6f7cf67c
Yusung <qkrdbtjd15@naver.com>
2022-03-11 14:53:51 +0900

    AptNotification (유저 알람) Entity

---
+++ house-batch/src/main/java/com/yusung/housebatch/core/Entity/AptNotification.java
@@ -0,0 +1,37 @@
+package com.yusung.housebatch.core.Entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "apt_notification")
+@EntityListeners(AuditingEntityListener.class)
+public class AptNotification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long aptNotificationId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String guLawdCd;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
