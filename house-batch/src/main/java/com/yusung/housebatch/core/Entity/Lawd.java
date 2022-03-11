56cd8b2e1fd391974dc828bc5962010c2ac0d7f6
Yusung <qkrdbtjd15@naver.com>
2022-03-11 14:52:12 +0900

    Lawd Entity

---
+++ house-batch/src/main/java/com/yusung/housebatch/core/Entity/Lawd.java
@@ -0,0 +1,43 @@
+package com.yusung.housebatch.core.Entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+// 엔티티에 JPA 어디팅 적용
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Table(name = "lawd")
+public class Lawd {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long lawdId;
+
+    @Column(nullable = false)
+    private String lawdCd;
+
+    @Column(nullable = false)
+    private String lawdDong;
+
+    @Column(nullable = false)
+    private Boolean exist;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
