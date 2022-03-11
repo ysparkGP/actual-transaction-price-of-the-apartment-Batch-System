f8f3288d87398c2f436fd32e23855a6bcfa108a7
Yusung <qkrdbtjd15@naver.com>
2022-03-11 14:55:16 +0900

    lawdCd (구 코드) Upsert Service

---
+++ house-batch/src/main/java/com/yusung/housebatch/core/service/LawdService.java
@@ -0,0 +1,25 @@
+package com.yusung.housebatch.core.service;
+
+import com.yusung.housebatch.core.Entity.Lawd;
+import com.yusung.housebatch.core.Repository.LawdRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class LawdService {
+
+    private final LawdRepository lawdRepository;
+
+    @Transactional
+    public void upsert(Lawd lawd){
+        // 데이터가 존재하면 수정, 데이터가 없으면 생성
+        Lawd saved = lawdRepository.findByLawdCd(lawd.getLawdCd())
+                .orElseGet(Lawd::new);
+        saved.setLawdCd(lawd.getLawdCd());
+        saved.setLawdDong(lawd.getLawdDong());
+        saved.setExist(lawd.getExist());
+        lawdRepository.save(saved);
+    }
+}
